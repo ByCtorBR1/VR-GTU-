68dfbc0d5aeae16327985d452ae1125e97754d31
ByCtorBR1 <148152866+ByCtorBR1@users.noreply.github.com>
2023-12-11 18:48:03 -0600

    navcambio

--- JS/nav.js
+++ JS/nav.js
@@ -1,5 +1,5 @@
 function openNav(){
-    document.getElementById("mobile").style.width = "100%";
+    document.getElementById("mobile").style.width = "60%";
 }
 function closeNav(){
     document.getElementById("mobile").style.width = "0%";
